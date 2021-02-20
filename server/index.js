dd5f056f07b93744551c0078e77a356722bfd9a3
Chetas Sharma <sharmachetas.111@gmail.com>
2021-02-20 13:26:11 +0530

    added socket events

--- server/index.js
+++ server/index.js
@@ -1,10 +1,13 @@
 const http = require("http");
 const express = require("express");
 const socketio = require("socket.io");
+const router = require("./router");
+const {addUser} = require("./methods");
 
 const PORT = process.env.PORT || 5000;
-const App = express();
-const server = http.createServer(App);
+const app = express();
+
+const server = http.createServer(app);
 const io = socketio(server, {
     cors: {
         origin: "https://localhost:3000",
@@ -13,9 +16,14 @@ const io = socketio(server, {
 });
 
 io.on("connection",(socket)=>{
-    socket.on("joinroom",(user,callback)=>{
-        const {name,room} = user;
-    })
+    socket.on("joinroom",({name,room},callback)=>{
+        const {error,user} = addUser({id: socket.id, name, room});
+        if(error) callback();
+        socket.emit("message",{user: "admin", text: `Welcome ${name} to ${room}!`});
+        socket.broadcast.to(user.room).emit("message",{user: "admin", text: `${name} joined`});
+        socket.join(user.room);
+    });
 });
 
-server.listen(PORT,()=>console.log(`Serevr is running on localhost:${PORT}`));
+app.use(router);
+server.listen(PORT,()=>console.log(`Server is running on localhost:${PORT}`));
