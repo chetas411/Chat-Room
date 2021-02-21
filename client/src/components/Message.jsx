5e60a3fc9cf9963ae71288b6b9788ee8ce1dd4d3
Chetas Sharma <sharmachetas.111@gmail.com>
2021-02-21 19:42:14 +0530

    added transition in messages

--- client/src/components/Message.jsx
+++ client/src/components/Message.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
+import Fade from '@material-ui/core/Fade';
 import Grid from '@material-ui/core/Grid';
 import Chip from '@material-ui/core/Chip';
 
 const Message = ({pos,message,user}) => {
     let color = (pos==="right")? "primary" : "secondary"
     return (
-        <ListItem key="1">
+        <Fade in={true}>
+        <ListItem>
             <Grid container>
                 <Grid item xs={12}>
                     <ListItemText align={pos} ><Chip color={color} size="medium" label={message} /></ListItemText>
@@ -17,6 +19,7 @@ const Message = ({pos,message,user}) => {
                 </Grid>
             </Grid>
         </ListItem>
+        </Fade>
     )
 }
 
