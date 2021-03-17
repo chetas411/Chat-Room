24605523a1006fb12aebb0922f17a53e7a1f6c30
Chetas Sharma <sharmachetas.111@gmail.com>
2021-03-17 12:57:09 +0530

    added routes for signup and login

--- server/router.js
+++ server/router.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const router = express.Router();
+const userController = require("./userController")
 
 router.get("/",(req,res)=>{
     res.send("Server is running")
 });
+
+router.post("/new",userController.create,userController.redirect);
+router.post("/login",userController.authenticate,userController.redirect);
 
 module.exports = router;
